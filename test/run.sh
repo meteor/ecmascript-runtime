1d4f675bce8c9cd04a461527655ffd898b71ee74
Ben Newman <ben@meteor.com>
2015-08-04 17:55:04 -0400

    Remove meteor-babel bits from test/run.sh.

--- test/run.sh
+++ test/run.sh
@@ -3,11 +3,7 @@
 cd $(dirname $0)
 TEST_DIR=$(pwd)
 
-BABEL_CACHE_DIR=${TEST_DIR}/.cache
-export BABEL_CACHE_DIR
-
 ${TEST_DIR}/../node_modules/mocha/bin/mocha \
     --reporter spec \
     --full-trace \
-    --compilers js:meteor-babel/register \
     tests.js
