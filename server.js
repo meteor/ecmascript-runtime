10cd77da661b40b253833481786085b7e4fdce86
Ben Newman <ben@meteor.com>
2015-08-04 19:04:53 -0400

    Reuse existing Map and Set implementations, if available.

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
-exports.Map = require("core-js/library/es6/map");
-exports.Set = require("core-js/library/es6/set");
+exports.Map = require("core-js/es6/map");
+exports.Set = require("core-js/es6/set");
 
 if (typeof window === "object") {
   Map = exports.Map;
