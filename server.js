a46e9cce831c7160c164d135de5f0ff661f6e6de
Ben Newman <bn@cs.stanford.edu>
2015-10-03 13:24:28 -0400

    Include Object, String, Function, and Symbol polyfills, too.

--- server.js
+++ server.js
@@ -1,9 +1,8 @@
+require("core-js/es6/object");
 require("core-js/es6/array");
+require("core-js/es6/string");
+require("core-js/es6/function");
 
+exports.Symbol = require("core-js/es6/symbol");
 exports.Map = require("core-js/es6/map");
 exports.Set = require("core-js/es6/set");
-
-if (typeof window === "object") {
-  Map = exports.Map;
-  Set = exports.Set;
-}
