a6383f1304d350c8ca379e32d8ff6e1762421bc6
Ben Newman <bn@cs.stanford.edu>
2015-10-03 14:02:02 -0400

    Export Symbol, Map, and Set globally.

--- server.js
+++ server.js
@@ -3,6 +3,6 @@ require("core-js/es6/array");
 require("core-js/es6/string");
 require("core-js/es6/function");
 
-exports.Symbol = require("core-js/es6/symbol");
-exports.Map = require("core-js/es6/map");
-exports.Set = require("core-js/es6/set");
+Symbol = exports.Symbol = require("core-js/es6/symbol");
+Map = exports.Map = require("core-js/es6/map");
+Set = exports.Set = require("core-js/es6/set");
